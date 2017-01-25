8a4c012db522c6c2c41f5b122d24e26714e276f5
pcurtis5688 <pcurtis5688@gmail.com>
2017-01-24 19:24:07 -0500

    730 midway

---
+++ app/src/main/java/com/fourtwelvedesign/dsmmeerut/database/SQLiteDBWrapper.java
@@ -0,0 +1,16 @@
+package com.fourtwelvedesign.dsmmeerut.database;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Paul Curtis on 1/24/2017.
+ */
+
+public class SQLiteDBWrapper {
+
+    private SQLiteDatabase sqLiteDatabase;
+
+    public void initDatabase() {
+        //db stuff here
+    }
+}
